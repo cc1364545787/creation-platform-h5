d707ebe10e2623037fac3fb0d31c0e4fda0052e1
lybics <1364545787@qq.com>
2024-01-29 10:31:23 +0800

    fix: 初始化

---
+++ my-app/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      Learn Reacsstp
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    <p>sss</p>
+    </div>
+  )
+}
+export default App;
